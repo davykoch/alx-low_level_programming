c358d70f7bef97f7dcc3926ef5091f53e86d1b10
davykoch <koechdavis24@gmail.com>
2023-08-24 17:00:19 +0300

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	while (h != NULL)
+	for (i = 0; h != NULL; i++)
 	{
 		if (h->str != NULL)
 		{
@@ -19,7 +19,6 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		}
 			h = h->next;
-			i++;
-	}
+}
 	return (i);
 }
