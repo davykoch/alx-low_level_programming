a58ce17a059c143a322195efe4049bac39a44c64
davykoch <koechdavis24@gmail.com>
2023-08-28 14:18:12 +0300

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0  ; Null-terminated string
-    newline db 10           ; Newline character ('\n')
-    
-section .text
-    global main
-
-extern printf
-
+          global    main
+          extern    printf
 main:
-    push rbp                ; Preserve the base pointer
-    mov rdi, hello          ; Format string for printf
-    call printf             ; Call printf with the format string
-    
-    mov rdi, newline        ; Format string for printf (newline)
-    call printf             ; Call printf with the newline format
-    
-    pop rbp                 ; Restore the base pointer
-    ret                     ; Return from main
+          mov   edi, format
+          xor   eax, eax
+          call  printf
+          mov 	eax, 0
+          ret
+format: db `Hello, Holberton\n`,0
