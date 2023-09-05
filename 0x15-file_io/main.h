28386ca67ce4a5a54d3e67479127fe88d9bb5308
davykoch <koechdavis24@gmail.com>
2023-09-05 22:03:01 +0300

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h> 
-#include <fcntl.h> 
+#include <unistd.h>
+#include <fcntl.h >
 #include <sys/types.h>
 #include <sys/stat.h>
 
